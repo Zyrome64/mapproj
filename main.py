13ab063335d7f18fa2de920be9b4b979ca977d92
TrueProgrammer <sasfomin@gmail.com>
2019-04-05 18:25:29 +0500

    Adding API Func

--- main.py
+++ main.py
@@ -189,4 +189,139 @@ def slideshow(coords_list):
 ##path = ['30.312300,59.941372', '30.313533,59.944456', '30.311844,59.946254', '30.294017,59.946655', '30.270461,59.953577', '30.264769,59.956766', '30.257398,59.957523', '30.212623,59.966225', '29.913233,59.891882']
 ##print(lonlat_distance(input('Введите долготу и широту первой точки через запятую - '), input('Введите долготу и широту второй точки через запятую - ')), 'м')
 ##pic('30.119060,59.932104', '0.2,0.2', 'map', 'pl=' + ','.join(path))
-slideshow([('-52.837122,47.191585', '0.001,0.001'), ('46.061245,51.529056', '0.001,0.001'), ('68.775195,38.564725', '0.05,0.05')])
+# slideshow([('-52.837122,47.191585', '0.001,0.001'), ('46.061245,51.529056', '0.001,0.001'), ('68.775195,38.564725', '0.05,0.05')])
+
+
+
+
+
+
+
+
+
+
+
+def sputnik_im(coord, spn, type, markers, lines):
+    response = None
+    try:
+        host = "http://static-maps.yandex.ru/1.x/?ll={}&spn={}&l={}".format(coord, spn, type)
+        if markers:
+            host += '&pt='
+            for i in range(len(markers)):
+                if i < len(markers) - 1:
+                    host += markers[i] + ',pm2rdm~'
+                else:
+                    host += markers[i] + ',pm2rdm'
+        if lines:
+            host += '&pl=c:ec473fFF,f:00FF00A0, w:7'
+            for x in lines:
+                host += ',' + x
+        map_request = host  # 0.002
+        # "http://static-maps.yandex.ru/1.x/?ll={}&spn={}&l={}&pt={},pm2rdm~{},pm2rdm~{},pm2rdm&pl=c:ec473fFF,f:00FF00A0, w:7,30.015249,59.865990,30.114517,59.946595,30.191421,59.970402,30.273819,59.952891,30.310705,59.945413"
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(geocoder_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+    except:
+        print("Запрос не удалось выполнить. Проверьте наличие сети Интернет.")
+        sys.exit(1)
+
+    # Запишем полученное изображение в файл.
+    map_file = "map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+        sys.exit(2)
+
+    # Инициализируем pygame
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    # Рисуем картинку, загружаемую из только что созданного файла.
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    # Переключаем экран и ждем закрытия окна.
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
+
+    # Удаляем за собой файл с изображением.
+    os.remove(map_file)
+
+
+
+class list_of_sputniks:
+    def __init__(self):
+        self.data = []
+        self.dat = []
+        self.index = 0
+    def appen(self, coord, spn, type):
+        response = None
+        try:
+            host = 'http://static-maps.yandex.ru/1.x/?ll={}&spn={}&l={}'.format(coord, spn, type)
+            map_request = host  # 0.002
+            response = requests.get(map_request)
+
+            if not response:
+                print("Ошибка выполнения запроса:")
+                print(geocoder_request)
+                print("Http статус:", response.status_code, "(", response.reason, ")")
+                sys.exit(1)
+        except:
+            print("Запрос не удалось выполнить. Проверьте наличие сети Интернет.")
+            sys.exit(1)
+
+        # Запишем полученное изображение в файл.
+        map_file = "map.png"
+        try:
+            with open(map_file, "wb") as file:
+                file.write(response.content)
+        except IOError as ex:
+            print("Ошибка записи временного файла:", ex)
+            sys.exit(2)
+        self.data.append(map_file)
+        self.dat.append(pygame.image.load(map_file))
+
+
+
+# Инициализируем pygame
+pygame.init()
+h = list_of_sputniks()
+h.appen('29.962672,59.943050', '0.3,0.3', 'sat')
+h.appen('29.962672,59.943050', '0.3,0.3', 'map')
+h.appen('29.962672,59.943050', '0.3,0.3', 'sat,skl')
+screen = pygame.display.set_mode((600, 450))
+flag = True
+while flag:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            flag = False
+            pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                h.index -= 1
+                if h.index < 0:
+                    h.index = len(h.dat) - 1
+            if event.key == pygame.K_RIGHT:
+                h.index += 1
+                if h.index > len(h.dat) - 1:
+                    h.index = 0
+    screen.blit(h.dat[h.index], (0, 0))
+    # Переключаем экран и ждем закрытия окна.
+    pygame.display.flip()
+pygame.quit()
+# Рисуем картинку, загружаемую из только что созданного файла.
+
+
+
+# sputnik_im('29.962672,59.943050', '0.3,0.3', 'map',
+#            ['37.439944,55.817950', '37.560154,55.791408', '37.554787,55.715562'],
+#            ['30.015249,59.865990', '30.114517,59.946595', '30.191421,59.970402', '30.273819,59.952891',
+#             '30.310705,59.945413'])
+
+
+
